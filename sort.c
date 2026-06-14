36b212323e9a4489e9d0abf90a1b6f9e3a7c6bc9
shakthibalankv <shakthibalankv@gmail.com>
2026-06-14 09:30:54 +0530

    Update sort.c

--- sort.c
+++ sort.c
@@ -1,5 +1,43 @@
 #include"header.h"
 
+struct st *clone_list(struct st *src)
+{
+    struct st *head = 0;
+    struct st *tail = 0;
+
+    while(src != 0)
+    {
+        struct st *node = (struct st *)malloc(sizeof(struct st));
+        node->roll       = src->roll;
+        strcpy(node->name, src->name);
+        node->percentage = src->percentage;
+        node->next       = 0;
+
+        if(head == 0)
+            head = tail = node;
+        else
+        {
+            tail->next = node;
+            tail       = node;
+        }
+        src = src->next;
+    }
+    return head;
+}
+
+void free_list(struct st *ptr)
+{
+    struct st *tmp;
+    while(ptr != 0)
+    {
+        tmp = ptr->next;
+        free(ptr);
+        ptr = tmp;
+    }
+}
+
+/* ---------- menu ---------- */
+
 void stud_sort(struct st **ptr)
 {
     char choice;
@@ -28,82 +66,72 @@ void stud_sort(struct st **ptr)
     }
 }
 
+/* ---------- sort by name (on a copy) ---------- */
+
 void sort_by_name(struct st **ptr)
 {
-    struct st *i;
-    struct st *j;
-    int   troll;
-    char  tname[100];
-    float tpct;
-
     if(*ptr == 0)
     {
         printf("List is empty.\n");
         return;
     }
 
-    for(i = *ptr; i != 0; i = i->next)
+    struct st *copy = clone_list(*ptr);
+
+    struct st *i, *j;
+    int   troll;
+    char  tname[100];
+    float tpct;
+
+    for(i = copy; i != 0; i = i->next)
     {
         for(j = i->next; j != 0; j = j->next)
         {
             if(strcmp(i->name, j->name) > 0)
             {
-                /* swap data */
-                troll      = i->roll;
-                strcpy(tname, i->name);
-                tpct       = i->percentage;
-
-                i->roll       = j->roll;
-                strcpy(i->name, j->name);
-                i->percentage = j->percentage;
-
-                j->roll       = troll;
-                strcpy(j->name, tname);
-                j->percentage = tpct;
+                troll = i->roll;  strcpy(tname, i->name);  tpct = i->percentage;
+                i->roll = j->roll; strcpy(i->name, j->name); i->percentage = j->percentage;
+                j->roll = troll;  strcpy(j->name, tname);  j->percentage = tpct;
             }
         }
     }
 
-    printf("List sorted by name.\n");
-    stud_show(*ptr);
+    printf("(Showing sorted view \n");
+    stud_show(copy);
+    free_list(copy);
 }
+
+/* ---------- sort by percentage (on a copy) ---------- */
 
 void sort_by_percentage(struct st **ptr)
 {
-    struct st *i;
-    struct st *j;
-    int   troll;
-    char  tname[100];
-    float tpct;
-
     if(*ptr == 0)
     {
         printf("List is empty.\n");
         return;
     }
 
-    for(i = *ptr; i != 0; i = i->next)
+    struct st *copy = clone_list(*ptr);
+
+    struct st *i, *j;
+    int   troll;
+    char  tname[100];
+    float tpct;
+
+    for(i = copy; i != 0; i = i->next)
     {
         for(j = i->next; j != 0; j = j->next)
         {
             if(i->percentage < j->percentage)
             {
-                /* swap data */
-                troll      = i->roll;
-                strcpy(tname, i->name);
-                tpct       = i->percentage;
-
-                i->roll       = j->roll;
-                strcpy(i->name, j->name);
-                i->percentage = j->percentage;
-
-                j->roll       = troll;
-                strcpy(j->name, tname);
-                j->percentage = tpct;
+                troll = i->roll;  strcpy(tname, i->name);  tpct = i->percentage;
+                i->roll = j->roll; strcpy(i->name, j->name); i->percentage = j->percentage;
+                j->roll = troll;  strcpy(j->name, tname);  j->percentage = tpct;
             }
         }
     }
 
-    printf("List sorted by percentage.\n");
-    stud_show(*ptr);
+    printf("(Showing sorted view \n");
+    stud_show(copy);
+    free_list(copy);
 }
